981b32cf5504ab7c9e21410a947d115f1bbad62f
Breno Ferreira <brenoleite1234@gmail.com>
2022-06-22 20:53:00 -0300

    Create app models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=50, null=False, unique=True)
+
+class Role(models.Model):
+    name = models.CharField(max_length=50, null=False)
+
+
+class Manager(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    phone = models.CharField(max_length=15, null=False)
+    city = models.ForeignKey('City', on_delete=models.CASCADE)
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    phone = models.CharField(max_length=15, null=False)
+    city = models.ForeignKey('City', on_delete=models.CASCADE)
+    role = models.ForeignKey('Role', on_delete=models.CASCADE)
